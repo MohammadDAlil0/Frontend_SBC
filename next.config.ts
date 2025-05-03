763b5b2cfe734b5ebfa2acd9489dd5e681f7d970
Mohammad Saeed Dalil <mohmaddalil5@gmail.com>
2025-05-03 13:41:48 +0300

    add allowedOrigin

--- next.config.ts
+++ next.config.ts
@@ -11,6 +11,10 @@ const nextConfig: NextConfig = {
     })
     return config
   },
+  allowedDevOrigins: [
+    "http://localhost:3002", // or your frontend URL
+    "http://147.93.87.131",  // IP if you're testing directly
+  ],
   // output: 'export',
 }
 
