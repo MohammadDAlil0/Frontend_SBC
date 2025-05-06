33d0ed1e89077911ace46865e2861d978d2d029d
Mohammad Saeed Dalil <mohmaddalil5@gmail.com>
2025-05-06 04:00:45 +0300

    last commit

--- next.config.ts
+++ next.config.ts
@@ -11,11 +11,6 @@ const nextConfig: NextConfig = {
     })
     return config
   },
-  allowedDevOrigins: [
-    "http://localhost:3002", // or your frontend URL
-    "http://147.93.87.131",  // IP if you're testing directly
-  ],
-  // output: 'export',
 }
 
 export default nextConfig
